508cb59268d940c86721dfb4cebd658377abdbc8
let_bibek <kumarbibek094@gmail.com>
2022-12-22 08:13:20 +0545

    Views

---
+++ app/src/main/java/com/softdrax/androidtut/Views_tut.java
@@ -0,0 +1,14 @@
+package com.softdrax.androidtut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Views_tut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_views_tut);
+    }
+}
