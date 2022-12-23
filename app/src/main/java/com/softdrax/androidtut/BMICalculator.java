a659eeed0b5b23d696857a83c613414543ae9b82
let_bibek <kumarbibek094@gmail.com>
2022-12-23 17:24:45 +0545

    BMI app done

--- app/src/main/java/com/softdrax/androidtut/BMICalculator.java
+++ app/src/main/java/com/softdrax/androidtut/BMICalculator.java
@@ -3,9 +3,12 @@ package com.softdrax.androidtut;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class BMICalculator extends AppCompatActivity {
 private int weight,heightFt,heightIn;
@@ -14,13 +17,34 @@ private int weight,heightFt,heightIn;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmicalculator);
 
-        TextView tvResult;
         EditText etWeight,etHeightFt,etHeightIn;
         Button btnCalc;
+        TextView tvRes;
 
         etWeight=findViewById(R.id.etWeight);
         etHeightFt=findViewById(R.id.etHeightFt);
         etHeightIn=findViewById(R.id.etHeightIn);
         btnCalc=findViewById(R.id.btnCalculate);
+        tvRes=findViewById(R.id.tvResultBmi);
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               weight= Integer.parseInt( etWeight.getText().toString());
+               heightFt=Integer.parseInt(etHeightFt.getText().toString());
+               heightIn=Integer.parseInt(etHeightIn.getText().toString());
+
+               int totalIn=heightFt*12+heightIn;
+               double heightCm=totalIn*2.53;
+               double totalHeightM=heightCm/100;
+               double BMI=weight/(Math.pow(totalHeightM,2));
+               if(BMI>25){
+                   tvRes.setText("You are over weight.");
+               }
+               else{
+                   tvRes.setText("You seem to be healthy.");
+               }
+            }
+        });
     }
+
 }
