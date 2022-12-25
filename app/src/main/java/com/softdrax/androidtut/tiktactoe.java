0420c75e312f0c17f9a18843414c06042276fba5
let_bibek <kumarbibek094@gmail.com>
2022-12-25 16:51:14 +0545

    first logic

--- app/src/main/java/com/softdrax/androidtut/tiktactoe.java
+++ app/src/main/java/com/softdrax/androidtut/tiktactoe.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class tiktactoe extends AppCompatActivity {
 Button ttcBtn1,ttcBtn2,ttcBtn3,ttcBtn4,ttcBtn5,ttcBtn6,ttcBtn7,ttcBtn8,ttcBtn9;
+String btnVAl1,btnVAl2,btnVAl3,btnVAl4,btnVAl5,btnVAl6,btnVAl7,btnVAl8,btnVAl9;
+TextView tvStatus;
+int flag=0;
+int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +23,7 @@ Button ttcBtn1,ttcBtn2,ttcBtn3,ttcBtn4,ttcBtn5,ttcBtn6,ttcBtn7,ttcBtn8,ttcBtn9;
     }
     private void init(){
         ttcBtn1=findViewById(R.id.tttBtn1);
-        ttcBtn3=findViewById(R.id.tttBtn2);
+        ttcBtn2=findViewById(R.id.tttBtn2);
         ttcBtn3=findViewById(R.id.tttBtn3);
         ttcBtn4=findViewById(R.id.tttBtn4);
         ttcBtn5=findViewById(R.id.tttBtn5);
@@ -28,6 +33,36 @@ Button ttcBtn1,ttcBtn2,ttcBtn3,ttcBtn4,ttcBtn5,ttcBtn6,ttcBtn7,ttcBtn8,ttcBtn9;
         ttcBtn9=findViewById(R.id.tttBtn9);
     }
     public void check(View view){
+        Button btnCurrent=(Button)view;
+        count++;
+        if(flag==0){
+            btnCurrent.setText("o");
+            flag=1;
+        }else{
+            btnCurrent.setText("x");
+            flag=0;
+        }
 
+        if(count>4){
+           btnVAl1=ttcBtn1.getText().toString();
+            btnVAl2=ttcBtn2.getText().toString();
+            btnVAl3=ttcBtn3.getText().toString();
+            btnVAl3=ttcBtn3.getText().toString();
+            btnVAl4=ttcBtn4.getText().toString();
+            btnVAl5=ttcBtn5.getText().toString();
+            btnVAl6=ttcBtn6.getText().toString();
+            btnVAl7=ttcBtn7.getText().toString();
+            btnVAl8=ttcBtn8.getText().toString();
+            btnVAl9=ttcBtn9.getText().toString();
+
+
+            if(btnVAl1.equals(btnVAl2)){
+                if(btnVAl1.equals(btnVAl3)){
+                    tvStatus.setText("Winner is: x");
+                }
+
+            }
+
+        }
     }
 }
