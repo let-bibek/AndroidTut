0d98c58962b3f80c58c2719b38c61f55340cec98
let_bibek <kumarbibek094@gmail.com>
2022-12-26 08:11:41 +0545

    intent passing

---
+++ app/src/main/java/com/softdrax/androidtut/intent_passing.java
@@ -0,0 +1,54 @@
+package com.softdrax.androidtut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class intent_passing extends AppCompatActivity {
+Button btnToTtt,btnToBmi,btnToViews;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_passing);
+        nextIntent();
+    }
+
+    private void nextIntent() {
+        btnToTtt=findViewById(R.id.btnToTtt);
+        btnToBmi=findViewById(R.id.btnToBmi);
+        btnToViews=findViewById(R.id.btnToViews);
+
+
+//        executes on clicking button btnNextIntent
+        btnToTtt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                intent constructor accepts the current activity class(intent_passing) and the next activity class(tiktactoe) where you want to navigate to
+                Intent nextIntent=new Intent(intent_passing.this,tiktactoe.class);
+
+                startActivity(nextIntent);
+            }
+        });
+
+//        bmi
+        btnToBmi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bmiIntent=new Intent(intent_passing.this,BMICalculator.class);
+                startActivity(bmiIntent);
+            }
+        });
+
+//        views
+        btnToViews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent btnToViews=new Intent(intent_passing.this,Views_tut.class);
+                startActivity(btnToViews);
+            }
+        });
+    }
+}
