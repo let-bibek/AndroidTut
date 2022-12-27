695bebefe1b8b235fe391a8ba77941d3b22d9d61
let_bibek <kumarbibek094@gmail.com>
2022-12-27 09:38:43 +0545

    simple animation

--- app/src/main/java/com/softdrax/androidtut/AnimationSamp.java
+++ app/src/main/java/com/softdrax/androidtut/AnimationSamp.java
@@ -11,13 +11,17 @@ import android.widget.TextView;
 
 public class AnimationSamp extends AppCompatActivity {
     TextView tvAnimation;
-    Button btnAnimTrans;
+    Button btnAnimTrans,btnAnimAlpha,btnAnimRot,btnAnimScale;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animationsamp);
         tvAnimation=findViewById(R.id.tvAnimation);
         btnAnimTrans=findViewById(R.id.btnAnimTrans);
+        btnAnimAlpha=findViewById(R.id.btnAnimAlpha);
+        btnAnimRot=findViewById(R.id.btnAnimRot);
+        btnAnimScale=findViewById(R.id.btnAnimScale);
+
         btnAnimTrans.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
@@ -25,5 +29,29 @@ public class AnimationSamp extends AppCompatActivity {
                tvAnimation.startAnimation(move);
            }
        });
+
+        btnAnimAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation alpha=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.alpha);
+                tvAnimation.startAnimation(alpha);
+            }
+        });
+
+        btnAnimRot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation rotation=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotation);
+                tvAnimation.startAnimation(rotation);
+            }
+        });
+
+        btnAnimScale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation scale=AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+                tvAnimation.startAnimation(scale);
+            }
+        });
     }
 }
