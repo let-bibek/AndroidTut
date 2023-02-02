ad09406ac7e0475ace3fa6935a0741cc184c32f9
let_bibek <kumarbibek094@gmail.com>
2023-02-02 14:28:29 +0545

    load image from gallery

--- app/src/main/java/com/softdrax/androidtut/ImageUploadUsingCamera.java
+++ app/src/main/java/com/softdrax/androidtut/ImageUploadUsingCamera.java
@@ -13,8 +13,9 @@ import android.widget.ImageView;
 
 public class ImageUploadUsingCamera extends AppCompatActivity {
     ImageView ivImageFromCamera;
-    Button btnLoadImageFromCamera;
-    private final int REQUEST_CODE = 100;
+    Button btnLoadImageFromCamera,btnLoadImageFromGallery;
+    private final int CAM_REQUEST_CODE = 100;
+    private final int GALLERY_REQ_CODE=200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +23,34 @@ public class ImageUploadUsingCamera extends AppCompatActivity {
         setContentView(R.layout.activity_image_upload_using_camera);
         ivImageFromCamera = findViewById(R.id.ivImageFromCamera);
         btnLoadImageFromCamera = findViewById(R.id.btnLoadImageFromCamera);
+        btnLoadImageFromGallery=findViewById(R.id.btnLoadImageFromGallery);
+
+//        load using camera
         btnLoadImageFromCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intentCamera = new Intent();
                 intentCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intentCamera, REQUEST_CODE);
+                startActivityForResult(intentCamera, CAM_REQUEST_CODE);
             }
         });
+
+
+//          load from gallery
+        btnLoadImageFromGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iGallery=new Intent();
+                iGallery.setAction(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery,GALLERY_REQ_CODE);
+            }
+        });
+
     }
+
+
+//    load using gallery
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -38,10 +58,17 @@ public class ImageUploadUsingCamera extends AppCompatActivity {
 
         if(resultCode==RESULT_OK){
 
-            if (requestCode==REQUEST_CODE){
+            if (requestCode==CAM_REQUEST_CODE){
                 // for camera
                 Bitmap bitmapImage=(Bitmap) data.getExtras().get("data");
                 ivImageFromCamera.setImageBitmap(bitmapImage);
+            }
+
+            if(requestCode==GALLERY_REQ_CODE){
+//                for gallery
+
+                ivImageFromCamera.setImageURI(data.getData());
+
             }
         }
     }
