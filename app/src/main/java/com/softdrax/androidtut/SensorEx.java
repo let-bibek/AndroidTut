82d243cf4227e91c7f23042975ee84a879e4f3c0
let_bibek <kumarbibek094@gmail.com>
2023-02-02 22:52:23 +0545

    biometric authentication

--- app/src/main/java/com/softdrax/androidtut/SensorEx.java
+++ app/src/main/java/com/softdrax/androidtut/SensorEx.java
@@ -1,17 +1,28 @@
 package com.softdrax.androidtut;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.Executor;
+
 public class SensorEx extends AppCompatActivity implements SensorEventListener {
     TextView tvSensorOutput;
+    BiometricPrompt biometricPrompt;
+    BiometricPrompt.PromptInfo promptInfo;
+    ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,9 @@ public class SensorEx extends AppCompatActivity implements SensorEventListener {
         setContentView(R.layout.activity_sensor_ex);
 
         tvSensorOutput = findViewById(R.id.tvSensorOutput);
+        constraintLayout = findViewById(R.id.layoutSensors);
+
+
         tvSensorOutput.setText("");
         SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -45,6 +59,54 @@ public class SensorEx extends AppCompatActivity implements SensorEventListener {
         } else {
             Toast.makeText(this, "Sensor not detected", Toast.LENGTH_SHORT).show();
         }
+
+
+        // biometric
+
+
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                Toast.makeText(this, "This device doesn't have fingerprint sensor", Toast.LENGTH_SHORT).show();
+
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                Toast.makeText(this, "Fingerprint busy", Toast.LENGTH_SHORT).show();
+
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                Toast.makeText(this, "No fingerprint is added yet", Toast.LENGTH_SHORT).show();
+        }
+
+        Executor executor = ContextCompat.getMainExecutor(this);
+
+        biometricPrompt = new BiometricPrompt(SensorEx.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(SensorEx.this, "Login Success", Toast.LENGTH_SHORT).show();
+                constraintLayout.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo
+                .Builder()
+                .setTitle("Use Fingerprint to login")
+                .setDescription("Use Fingerprint to login")
+                .setDeviceCredentialAllowed(true)
+                .build();
+
+        biometricPrompt.authenticate(promptInfo);
+
+
     }
 
     @Override
@@ -74,4 +136,6 @@ public class SensorEx extends AppCompatActivity implements SensorEventListener {
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+
 }
